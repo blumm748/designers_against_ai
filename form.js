e649d9bdd62a5f8d3f38751c91f47fd097020d70
blumm748 <blumm748@newschool.edu>
2026-05-14 01:45:43 -0400

    Web3Forms fixed (maybe)

--- form.js
+++ form.js
@@ -1,38 +1,23 @@
-const form = document.getElementById('form');
-const result = document.getElementById('result');
 
-form.addEventListener('submit', function(e) {
-  e.preventDefault();
-  const formData = new FormData(form);
-  const object = Object.fromEntries(formData);
-  const json = JSON.stringify(object);
-  result.innerHTML = "Please wait..."
+  const form = document.getElementById('form');
+  const result = document.getElementById('result');
 
-    fetch('https://api.web3forms.com/submit', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-            },
-            body: json
-        })
-        .then(async (response) => {
-            let json = await response.json();
-            if (response.status == 200) {
-                result.innerHTML = json.message;
-            } else {
-                console.log(response);
-                result.innerHTML = json.message;
-            }
-        })
-        .catch(error => {
-            console.log(error);
-            result.innerHTML = "Something went wrong!";
-        })
-        .then(function() {
-            form.reset();
-            setTimeout(() => {
-                result.style.display = "Successfully submitted :)";
-            }, 3000);
-        });
-});
+  form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(form);
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      result.innerHTML = "Successfully submitted :)";
+      form.reset();
+    } else {
+      result.innerHTML = "Something went wrong. Please try again :(";
+    }
+  });
+
